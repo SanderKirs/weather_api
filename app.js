1c76dd66414b8ac0940258d7af0925de411b4bf4
SanderKirs <sander.kirs@khk.ee>
2022-03-07 12:54:03 +0200

    Displayed city weather data from API.

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ function weatherDataFetch(city){
         return resp.json()
     }).then(function(data){
         console.log(data);
+        drawWeather(data);
     }).catch(function (){
     });
 }
 function cityWeather(e){
     weatherDataFetch('Tallinn');
+}
+function drawWeather (data){
+    var celcius = Math.round(parseFloat(data.main.temp) -273.15);
+    var description = data.weather[0].description;
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + '&deg';
+    document.querySelector('#location').innerHTML = data.name;
 }
